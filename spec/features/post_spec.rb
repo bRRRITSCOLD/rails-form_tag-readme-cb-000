72c907ab126a48f00ab01b913d68466858060056
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-08 02:36:38 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe 'new post' do
-
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
@@ -12,4 +11,15 @@ describe 'new post' do
     expect(page).to have_content('Post Form')
   end
 
+  it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+    visit new_post_path
+    fill_in 'post_title', with: 'My post title'
+    fill_in 'post_description', with: 'My post description'
+
+    click_on 'Submit Post'
+
+    expect(page.current_path).to eq(posts_path)
+    expect(page).to have_content('My post title')
+    expect(page).to have_content('My post description')
+  end
 end
